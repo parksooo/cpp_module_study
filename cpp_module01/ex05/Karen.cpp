8881c37e4da1137ab0128d770f61779188ed13b2
Suhwan Park <suhwpark@c4r1s2.42seoul.kr>
2023-06-05 19:53:29 +0900

    cpp01 ex05 ex06 fix std::cout

--- cpp_module01/ex05/Karen.cpp
+++ cpp_module01/ex05/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: suhwpark <suhwpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 18:21:22 by suhwpark          #+#    #+#             */
-/*   Updated: 2023/06/05 19:27:26 by suhwpark         ###   ########.fr       */
+/*   Updated: 2023/06/05 19:51:59 by suhwpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,25 @@
 
 void	Karen::debug()
 {
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
 }
 
 void	Karen::info()
 {
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
 }
 
 void	Karen::warning()
 {
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
 }
 
 void	Karen::error()
 {
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
 }
 
--- cpp_module01/ex06/main.cpp
+++ cpp_module01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: suhwpark <suhwpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 18:21:24 by suhwpark          #+#    #+#             */
-/*   Updated: 2023/06/05 19:40:18 by suhwpark         ###   ########.fr       */
+/*   Updated: 2023/06/05 19:53:10 by suhwpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,7 @@ int main()
 	Karen k;
 
 	k.complain("DEBUG");
-	std::cout << "-----Other Test Case!" << std::endl;
 	k.complain("INFO");
-	std::cout << "-----Other Test Case!" << std::endl;
 	k.complain("WARNING");
-	std::cout << "-----Other Test Case!" << std::endl;
 	k.complain("ERROR");
 }
