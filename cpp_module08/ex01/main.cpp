0317b17845495060cbc415d32fb0271c4c5498b6
parksooo <popomarin28@gmail.com>
2023-09-13 21:33:04 +0900

    [feat] : add main

--- cpp_module08/ex01/main.cpp
+++ cpp_module08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: suhwpark <suhwpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 21:22:27 by suhwpark          #+#    #+#             */
-/*   Updated: 2023/09/13 21:26:08 by suhwpark         ###   ########.fr       */
+/*   Updated: 2023/09/13 21:32:49 by suhwpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,22 @@ int main() {
         std::cout << sp.longestSpan() << std::endl;
     } catch(std::exception &e) {
         std::cout << e.what() << std::endl;
+    }
+
+    std::cout << "------ span ------" << std::endl;
+    Span span(10000);
+    for (int i = 1; i <= 9998; i++)
+        span.addNumber(i);
+    span.addNumber(9998);
+    span.addNumber(100000);
+    std::cout << span.shortestSpan() << std::endl;
+    std::cout << span.longestSpan() << std::endl;
+    try
+    {
+        span.addNumber(-1);
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << "\n";
     }
 }
