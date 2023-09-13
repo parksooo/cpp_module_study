92da13aaf0e0a210934e1f4acfff74a87b7e8a08
parksooo <popomarin28@gmail.com>
2023-09-13 14:09:02 +0900

    [feat] : start cpp06 ex00

---
+++ cpp_module06/ex00/ScalarTpye.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarTpye.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suhwpark <suhwpark@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 13:56:56 by suhwpark          #+#    #+#             */
+/*   Updated: 2023/09/13 14:07:40 by suhwpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARTYPE_HPP
+# define SCALARTYPE_HPP
+# include <iostream>
+
+class ScalarTpye {
+    private :
+            std::string _str;
+            int _int;
+            float _float;
+            double _double;
+    public  :
+}
+#endif
