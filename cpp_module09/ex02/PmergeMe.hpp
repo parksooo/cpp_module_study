881dc33e248b63888da227e40c362251345107c5
parksooo <popomarin28@gmail.com>
2023-09-18 14:45:37 +0900

    [feat] : vector

---
+++ cpp_module09/ex02/PmergeMe.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suhwpark <suhwpark@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 14:16:21 by suhwpark          #+#    #+#             */
+/*   Updated: 2023/09/18 14:44:35 by suhwpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+# include <iostream>
+# include <vector>
+# include <sstream>
+#endif
--- cpp_module09/ex02/main.cpp
+++ cpp_module09/ex02/main.cpp
@@ -6,7 +6,53 @@
 /*   By: suhwpark <suhwpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 14:16:23 by suhwpark          #+#    #+#             */
-/*   Updated: 2023/09/17 14:30:45 by suhwpark         ###   ########.fr       */
+/*   Updated: 2023/09/18 14:42:35 by suhwpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+int    validateArgumentsValues(std::string n) {
+    char *ptr;
+    double _value = std::strtod(n.c_str(), &ptr);
+
+    if (_value == 0.0 && !std::isdigit(n[1]))
+        return -1;
+    if (*ptr && std::strcmp("f", ptr))
+        return -1;
+    if (_value < 0)
+        return -1;
+    return static_cast<int>(_value);
+}
+
+int main(int ac, char **av)
+{
+	std::vector<int> valueVector;
+	
+    if (ac < 2) {
+        std::cout << "Error." << std::endl;
+        return 1;
+    }
+    int i = 1;
+    while (i < ac) {
+        std::string argv(av[i]);
+        std::istringstream ss(argv);
+        std::string num;
+        
+        while (std::getline(ss, num, ' ')) {
+			int _value = 0;
+			if (num.empty())
+				continue ;
+			_value = validateArgumentsValues(num);
+            if (!num.empty() && _value < 0) {
+                std::cout << "Error." << std::endl;
+                return 1;
+            }
+			valueVector.push_back(_value);			
+        }
+		i++;
+    }
+	for (std::vector<int>::iterator iter = valueVector.begin(); iter != valueVector.end(); iter++) {
+		std::cout << *iter << std::endl;
+	}
+}
